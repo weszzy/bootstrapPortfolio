11e5673c5462569a84b7c85a2112c2c3e0ab9259
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2022-05-31 01:43:15 -0300

    added columns; scrumCertif and other minor changes

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function myFunction2() {
         x.style.display = "none";
     }
 }
+
+function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
